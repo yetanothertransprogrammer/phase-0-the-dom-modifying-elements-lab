bdd403117d46e4fff5f7f1e2b6bf3a15318963f2
Brynfor Morris <brynfor.morris@gmail.com>
2023-01-18 11:13:51 -0600

    commit tree f506e071e75f45a5d6d4c3be7da1098b6b8794af

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const body = document.getElementsByTagName("body")[0];
+const main = document.getElementById("main");
+body.removeChild(main);
+
+let newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+
+newHeader.id = "victory";
+
+newHeader.textContent = "Bryn is the champion";
